4641dc753f5329b1d74cc96fbaeceb28fe63d490
Marc Lewis <marc@marclewis.io>
2022-09-06 14:00:20 +0100

    Feat: get a working solution for add-two-numbers

--- problems/add-two-numbers/js/addTwoNumbers.js
+++ problems/add-two-numbers/js/addTwoNumbers.js
@@ -1,30 +1,62 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
+ * @type {{val: number, next: ListNode}} ListNode
  */
 
- function getNum(node) {
-  const placeValue = 1;
-  const num = 0;
+/**
+ * Create a ListNode
+ * @param {number} val
+ * @param {ListNode} next
+ */
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+
+/**
+ * Get the number from a ListNode
+ * @param {ListNode} node
+ * @returns
+ */
+function getNum(node) {
+  let placeValue = 1;
+  let num = 0;
   let currentNode = node;
-  while(currentNode.next) {
-      num += (this.val * placeValue);
+  while(currentNode) {
+      num += (currentNode.val * placeValue);
       placeValue *= 10;
-      currentNode = node.next;
+      currentNode = currentNode.next;
   }
   return num;
 }
 
 /**
-* @param {ListNode} l1
-* @param {ListNode} l2
-* @return {ListNode}
-*/
+ * Yikes
+ * @param {number} num
+ * @returns {ListNode}
+ */
+function nodify (num) {
+  let nodes = []; // mad closure
+  let i = 0;
+  while(num) {
+      const nodeValue = num % 10;
+      num = Math.floor(num / 10);
+      nodes[i] = new ListNode(nodeValue);
+      if(nodes[i - 1]) {
+          nodes[i - 1].next = nodes[i]
+      }
+      i++;
+  }
+  return nodes[0];
+}
+
+
+/**
+ * Get the reversed ListNode number from the addition of two ListNodes
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
 function addTwoNumbers (l1, l2) {
   const sum = getNum(l1) + getNum(l2);
+  return nodify(sum);
 };
-
-export { addTwoNumbers };
--- problems/add-two-numbers/js/addTwoNumbers.spec.js
+++
@@ -1,7 +0,0 @@
-import { addTwoNumbers } from "./addTwoNumbers";
-
-describe("addTwoNumbers", () => {
-  it("should add two numbers", () => {
-    expect(addTwoNumbers(2, 3)).toEqual(5);
-  });
-})
